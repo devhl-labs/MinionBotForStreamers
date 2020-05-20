99c02c978395890ee4cb9c8b027e938012dd7073
devhl-labs <shawnkanyer@gmail.com>
2020-05-20 11:56:26 -0400

    removed a debugging error

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -45,7 +45,6 @@ namespace TwitchOverlayConsoleAppCore
             try
             {
                 Update(false);
-                throw new Exception();
             }
             catch (Exception err)
             {
